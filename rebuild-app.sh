974f968b20504a0da4c921cf9bf8d6f1c6613557
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-01-22 00:07:33 +0100

    updated

--- rebuild-app.sh
+++ rebuild-app.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+(
 "$SCRIPT_DIR/launcher" rebuild app
 docker exec app bash -c '[[ "$(cat /etc/hosts|grep wp.test.mpop)" != "" ]] || echo "$(cat /etc/hosts|grep 172|xargs -n 1|sed -n 1p|sed -E "s/(^[0-9]+\.[0-9]+\.[0-9]+\.)[0-9]+/\11/") wp.test.mpop" >> /etc/hosts'
+) >> "$SCRIPT_DIR/launcher.log" 2>&1 &
+
+echo "Script lanciato in background con PID: $!"
